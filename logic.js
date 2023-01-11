1b7c0d3a12486e3a2137c156531e75a4a71e9537
Shai0104 <114080197+Shai0104@users.noreply.github.com>
2023-01-11 14:27:29 +0100

    hej

--- logic.js
+++ logic.js
@@ -17,7 +17,7 @@ actions.add1 = {
 actions.sortOnLength = {
     types: [String, String, String],
     function: function(a, b, c){
-        return "a aa aaa";
+        return "a bb ccc";
     }    
 }
 
